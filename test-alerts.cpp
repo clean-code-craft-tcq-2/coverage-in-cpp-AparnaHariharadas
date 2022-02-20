64ac69dd87ec42d301939005db330610a0fe6aa7
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-02-20 12:02:36 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -4,49 +4,49 @@
 #include "typewise-alert.h"
 
 TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
-  REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
-  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+  REQUIRE(inferTempBreachTypeUsingLimits(double(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferTempBreachTypeUsingLimits(35, 20, 30) == TOO_HIGH);
+  REQUIRE(inferTempBreachTypeUsingLimits(25, 20, 30) == NORMAL);
   
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,50) == TOO_HIGH);
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,-20) == TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,20) == NORMAL);
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,45) == TOO_HIGH);
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,20) == NORMAL);
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,-10) == TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,10) == NORMAL);
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,-5) == TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,40) == TOO_HIGH);
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,10) == NORMAL);
-  REQUIRE(classifyTemperatureBreach((CoolingType)7,10) == INVALID);
+  REQUIRE(classifyTemperatureBreachType(HI_ACTIVE_COOLING,50) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreachType(HI_ACTIVE_COOLING,-20) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreachType(HI_ACTIVE_COOLING,20) == NORMAL);
+  REQUIRE(classifyTemperatureBreachType(MED_ACTIVE_COOLING,45) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreachType(MED_ACTIVE_COOLING,20) == NORMAL);
+  REQUIRE(classifyTemperatureBreachType(MED_ACTIVE_COOLING,-10) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreachType(PASSIVE_COOLING,10) == NORMAL);
+  REQUIRE(classifyTemperatureBreachType(PASSIVE_COOLING,-5) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreachType(PASSIVE_COOLING,40) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreachaType(PASSIVE_COOLING,10) == NORMAL);
+  REQUIRE(classifyTemperatureBreachType((CoolingType)7,10) == INVALID);
   
   BatteryCharacter BatteryChar;
-  checkAndAlert(TO_EMAIL,BatteryChar,40);
-  checkAndAlert((AlertTarget)7,BatteryChar,40);
+  checkBatteryTempForBreachAndAlertTarget(TO_EMAIL,BatteryChar,40);
+  checkBatteryTempForBreachAndAlertTarget((AlertTarget)7,BatteryChar,40);
   BatteryChar.coolingType = PASSIVE_COOLING;
-  checkAndAlert(TO_CONTROLLER,BatteryChar,30);
+  checkBatteryTempForBreachAndAlertTarget(TO_CONTROLLER,BatteryChar,30);
   BatteryChar.coolingType = HI_ACTIVE_COOLING;
-  checkAndAlert(TO_EMAIL,BatteryChar,50);
-  checkAndAlert(TO_EMAIL,BatteryChar,20);
-  checkAndAlert(TO_EMAIL,BatteryChar,-20);
+  checkBatteryTempForBreachAndAlertTarget(TO_EMAIL,BatteryChar,50);
+  checkBatteryTempForBreachAndAlertTarget(TO_EMAIL,BatteryChar,20);
+  checkBatteryTempForBreachAndAlertTarget(TO_EMAIL,BatteryChar,-20);
   BatteryChar.coolingType = MED_ACTIVE_COOLING;
-  checkAndAlert(TO_EMAIL,BatteryChar,45);
-  checkAndAlert(TO_EMAIL,BatteryChar,20);
-  checkAndAlert(TO_EMAIL,BatteryChar,-10);
+  checkBatteryTempForBreachAndAlertTarget(TO_EMAIL,BatteryChar,45);
+  checkBatteryTempForBreachAndAlertTarget(TO_EMAIL,BatteryChar,20);
+  checkBatteryTempForBreachAndAlertTarget(TO_EMAIL,BatteryChar,-10);
   BatteryChar.coolingType = PASSIVE_COOLING;
-  checkAndAlert(TO_EMAIL,BatteryChar,40);
-  checkAndAlert(TO_EMAIL,BatteryChar,20);
-  checkAndAlert(TO_EMAIL,BatteryChar,-20);
+  checkBatteryTempForBreachAndAlertTarget(TO_EMAIL,BatteryChar,40);
+  checkBatteryTempForBreachAndAlertTarget(TO_EMAIL,BatteryChar,20);
+  checkBatteryTempForBreachAndAlertTarget(TO_EMAIL,BatteryChar,-20);
   //BatteryChar.coolingType = NULL;
-  checkAndAlert(TO_EMAIL,BatteryChar,40);
-  checkAndAlert(TO_EMAIL,BatteryChar,20);
-  checkAndAlert(TO_EMAIL,BatteryChar,-20);
+  checkBatteryTempForBreachAndAlertTarget(TO_EMAIL,BatteryChar,40);
+  checkBatteryTempForBreachAndAlertTarget(TO_EMAIL,BatteryChar,20);
+  checkBatteryTempForBreachAndAlertTarget(TO_EMAIL,BatteryChar,-20);
   BatteryChar.coolingType = (CoolingType)7;
-  checkAndAlert(TO_EMAIL,BatteryChar,40);
+  checkBatteryTempForBreachAndAlertTarget(TO_EMAIL,BatteryChar,40);
   
-  sendToEmail(TOO_LOW);
-  sendToEmail(TOO_HIGH);
-  sendToEmail(NORMAL);
+  sendBreachTypeToEmail(TOO_LOW);
+  sendBreachTypeToEmail(TOO_HIGH);
+  sendBreachTypeToEmail(NORMAL);
   
   
 }

bfa127f1a84c61a8917558272d25b707eb3dfb84
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-02-21 11:01:08 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,35 +1,6 @@
 #include "typewise-alert.h"
 #include <stdio.h>
-#include <vector>
-#include <sstream>
-
-/*map<CoolingType, int> lowerLimitMapper {
-{PASSIVE_COOLING,0},
-{HI_ACTIVE_COOLING,0},
-{MED_ACTIVE_COOLING,0},
-};
-map<CoolingType, int> upperLimitMapper {
-{PASSIVE_COOLING,35},
-{HI_ACTIVE_COOLING,45},
-{MED_ACTIVE_COOLING,40},
-};*/
-map<CoolingType, std::vector<int>> temperatureUpperAndLowerLimitsMapper {
-{PASSIVE_COOLING, {0, 35}},
-{HI_ACTIVE_COOLING, {0, 45}},
-{MED_ACTIVE_COOLING, {0,40}}
-};
-
-map<BreachType,const char*> temperatureBreachMapper {
-{TOO_LOW,"Temperature is too low"},
-{TOO_HIGH,"Temperature is too high"},
-{NORMAL,"Temperature is normal"},
-{INVALID,"Invalid Cooling type is given please check"},
-};
-using pfunc = void (*)(BreachType);
-map<AlertTarget, pfunc> alertTargetFuncFinder {
-{TO_CONTROLLER, sendBreachTypeToController},
-{TO_EMAIL, sendBreachTypeToEmail}
-};
+#include "tempBreachAlertmapper.h"
 
 BreachType inferTempBreachTypeUsingLimits(double value, double lowerLimit, double upperLimit) {
   return (value < lowerLimit ? TOO_LOW : (value > upperLimit ? TOO_HIGH : NORMAL));
@@ -38,14 +9,8 @@ BreachType inferTempBreachTypeUsingLimits(double value, double lowerLimit, doubl
 
 BreachType classifyTemperatureBreachType(
     CoolingType coolingType, double temperatureInC) {
-  //int lowerLimit = 0;
-  //int upperLimit = 0;
-  //auto iterLower = lowerLimitMapper.find(coolingType);
-  if (temperatureUpperAndLowerLimitsMapper.find(coolingType) != temperatureUpperAndLowerLimitsMapper.end()){
-  //if(iterLower != lowerLimitMapper.end()){
-    //lowerLimit = iterLower->second;
+  if (temperatureUpperAndLowerLimitsMapper.find(coolingType) != temperatureUpperAndLowerLimitsMapper.end())
     return inferTempBreachTypeUsingLimits(temperatureInC,temperatureUpperAndLowerLimitsMapper[coolingType].at(0), temperatureUpperAndLowerLimitsMapper[coolingType].at(1));
-    //return inferTempBreachTypeUsingLimits(temperatureInC, temperatureUpperAndLowerLimitsMapper[coolingType].at[0], upperLimitMapper.find(coolingType)->second);
   }
     else
       return INVALID;
@@ -57,15 +22,7 @@ void checkBatteryTempForBreachAndAlertTarget(
   BreachType breachType = classifyTemperatureBreachType(
     batteryChar.coolingType, temperatureInC
   );
-
-  /*switch(alertTarget) {
-    case TO_CONTROLLER:
-      sendBreachTypeToController(breachType);
-      break;
-    case TO_EMAIL:
-      sendBreachTypeToEmail(breachType);
-      break;
-  }*/
+  
   pfunc getTarget = alertTargetFuncFinder[alertTarget];
   (*getTarget)(breachType);
   
